2d5cbd3d5e1729bc5cb7c1a87127e928e5bda7c3
MaksimOstrouhov <Kristina199042Smirnova@gmail.com>
2019-08-23 22:44:44 +0200

    refacto(back): Reorganize :recycle:

--- api/api/settings.py
+++ api/api/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
