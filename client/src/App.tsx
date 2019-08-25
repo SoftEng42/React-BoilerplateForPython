5b3fe678ca2938ef0d426c42faf49d8165dbde08
MaksimOstrouhov <Kristina199042Smirnova@gmail.com>
2019-08-25 14:58:19 +0200

    :airplane: Map Login and Home pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { ConnectedRouter } from "connected-react-router";
+import { routerHistory } from "./redux/Router/Router.history";
+import { Route, Switch } from 'react-router-dom';
+import { ROUTE } from "./redux/Router/Router.routes";
+import LoginPage from "./pages/login/Login.wrap";
+import HomePage from "./pages/home/Home.wrap";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div>
-        Hello world!
-      </div>
+      <ConnectedRouter history={routerHistory}>
+        <Switch>
+          <Route path={ROUTE.LOGIN} component={LoginPage} />
+          <Route path={ROUTE.HOME} component={HomePage} />
+        </Switch>
+      </ConnectedRouter>
     </Provider>
   );
-}
+};
 
 export default App;
