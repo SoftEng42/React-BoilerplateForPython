ce2a69899d314af3fe351b6d9d1672cf42240727
MaksimOstrouhov <Kristina199042Smirnova@gmail.com>
2019-09-03 15:17:44 +0200

    :sparkles: Add real connexion to api

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -123,12 +125,11 @@ ENV_PATH = os.path.abspath(os.path.dirname(__file__))
 MEDIA_ROOT = "/usr/src"
 MEDIA_URL = "api/media/"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
-    }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
+
+CSRF_COOKIE_NAME = "XSRF-TOKEN"
--- client/src/redux/User/User.api.ts
+++ client/src/redux/User/User.api.ts
@@ -1,16 +1,23 @@
 import { User, Credentials } from "./User.types";
+import { Api } from "../../services/api";
 
-export function makeLoginRequest({ username }: Credentials): User {
-  return { 
-    username: username,
-  };
+const API_ROUTES = {
+  LOGIN: '/api-auth/login/',
+  LOGOUT: '/api-auth/logout',
+  RESET_PASSWORD: '/api-auth/password',
+};
+
+export async function makeLoginRequest(credentials: Credentials): Promise<User> {
+  await Api().post(API_ROUTES.LOGIN, credentials);
+  return { username: credentials.username }
 }
 
-export function makeLogoutRequest() {
+export async function makeLogoutRequest() {
+  await Api().post(API_ROUTES.LOGOUT);
   return;
 }
-export function makeForgotPasswordRequest(email: String) {
-  return;
+export async function makeForgotPasswordRequest(email: String) {
+  return Api().post(API_ROUTES.RESET_PASSWORD);
 }
 
 export function makeResetPasswordRequest(password: String, token: String) {
